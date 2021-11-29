b4db68e0d96a659db338e035003e6d2ea28d3870
Emmanuel Torres <torresvazquez_emmanuel@outlook.com>
2021-11-29 08:05:24 -0600

    Filtrado de datos y debuggin, sintaxisError

--- filtrando_datos.py
+++ filtrando_datos.py
@@ -74,12 +74,23 @@ DATA = [
 def run():
 
     # Comprehensions solutions
-    all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
-    all_Platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
-    adults =  [worker["name"] for worker in DATA if worker["age"] > 18]
-    old_people = list(map(lambda worker: worker | {"old": worker["age"] > 70}, DATA))
+    # all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
+    # all_Platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
+    # adults =  [worker["name"] for worker in DATA if worker["age"] > 18]
+    # old_people = list(map(lambda worker: worker | {"old": worker["age"] > 70}, DATA))
 
-    for worker in all_python_devs:
+    # Lambda y High Order solutions
+
+    # all_python_devs = list(filter(lambda worker: worker["language"] == "python",DATA))
+    # all_python_devs = list(map(lambda worker: worker["name"],all_python_devs))
+    all_Platzi_workers = list(filter(lambda worker: worker["organization"]=="Platzi",DATA))
+    all_Platzi_workers = list(map(lambda worker: worker["name"], all_Platzi_workers))
+    # adults = list(filter(lambda worker: worker["age"] > 18, DATA ))
+    # adults = list(map(lambda worker: worker["name"], adults))
+    # old_people = list(map(lambda worker: worker | {"old": worker["age"] >70},DATA))
+
+
+    for worker in all_Platzi_workers:
         print(worker)
 
 
